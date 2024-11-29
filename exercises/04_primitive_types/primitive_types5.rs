38b0b1532b892556b4a87237488964e63a208be1
rledford <ryan.c.ledford@gmail.com>
2024-11-28 22:53:04 -0600

    Initial commit. Completed 00_intro through 12_options, quiz1 and quiz2.

---
+++ exercises/04_primitive_types/primitive_types5.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let cat = ("Furry McFurson", 3.5);
+
+    // TODO: Destructure the `cat` tuple in one statement so that the println works.
+    let (name, age) = cat;
+    println!("{name} is {age} years old");
+}
